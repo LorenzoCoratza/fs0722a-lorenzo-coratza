4aceed598d05bfcdc6c23c3bfc56343a02021f66
LorenzoCoratza <lorenzocoratza@gmail.com>
2022-10-31 14:04:38 +0100

    compiti w2 d1

---
+++ m1/s2/d1/js/index.js
@@ -0,0 +1,4 @@
+alert('Usa il pop up per dare dei messaggi');
+alert('prova 1 alert');
+window.alert('Sto studiando JS');
+window.alert('prova comando window');
---
+++ m1/s2/d1/js/naming.js
@@ -0,0 +1,30 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+console.log($pet)
+
+
+console.log(_pet)
+
+console.log(_pet2 + _pet)
+
+
+
+var prodotto = 50
+
+var iva= prodotto /100 * 22;
+
+console.log(prodotto + iva);
+//var 12 = 'numero';
