f9a5147fc5d0b8d40d444eda92b6aa4ac2e64909
LorenzoCoratza <lorenzocoratza@gmail.com>
2022-12-20 18:02:18 +0100

    esercizio d2

--- m3/s1/d1/esercizio1.ts
+++ m3/s1/d1/esercizio1.ts
@@ -10,16 +10,16 @@ function giocatoriRandomNumberGeneratorrrrr():void{
     
     let g2 = 7;
     
-    let rnd = Math.floor(Math.random() * 99 + 1) 
+    let rdm = Math.floor(Math.random() * 99 + 1) 
     
     console.log("attualmente stanno giocando: ",g1,g2);
 
     setTimeout(function() {
-        if(g1 == rnd ||g2 ==rnd){
+        if(g1 == rdm ||g2 ==rdm){
             console.log("uno dei 2 ha indovinato...");
 
             setTimeout(function() {
-                if(g1 == rnd){
+                if(g1 == rdm){
                     console.log("il primo giocatore ha vinto la sessione");
                 }else{
                     console.log("il secondo giocatore ha vinto la partita");
@@ -28,8 +28,8 @@ function giocatoriRandomNumberGeneratorrrrr():void{
         }else{
             console.log("nessuno ha vinto ma il giocatore che si è avvicinato di più è...")
             setTimeout(function() {
-                let diff1 = Math.abs(g1 - rnd);
-                let diff2 =Math.abs(g2 - rnd);
+                let diff1 = Math.abs(g1 - rdm);
+                let diff2 =Math.abs(g2 - rdm);
 
                 Math.min(diff1,diff2) == diff1 ? console.log("giocatore1") : console.log("giocatore2");
             },2000);
